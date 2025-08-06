53ed17f539ad53deb0a3aab0428fd84345eb3543
Luigi9665 <ventriglia.luigi@hotmail.com>
2025-08-07 00:49:39 +0200

    aggiunta dei tasti reset all e remove all. stile e strutura html completato. codice ottimizzato

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,79 +3,117 @@ const textInput = document.querySelector("input");
 // const button = document.querySelector("button");
 const insertLi = document.querySelector("ul");
 
+// form.addEventListener("submit", function (e) {
+//   // PER PRIMISSIMA COSA (SEMPRE) dovremo fermare il comportamento di default del form (il refresh) in questo modo:
+//   e.preventDefault();
+//   // dobbiamo crearci un div che conterrà la lista
+//   const newDiv = document.createElement("div");
+//   //assegnarci una classe per lo stile
+//   newDiv.classList.add("div-lista");
+//   // dobbiamo prenderci il dato dell'input
+//   //   textInput.value = textInput.value.trim();
+//   if (textInput.value.trim() === "") {
+//     //messaggio di errore in caso di stringa vuota
+//     alert("ATTENZIONE! Non hai inserito alcun testo!");
+//   } else {
+//     //comandi per la creazione di una nuova lista da inserire in un div
+//     const newList = document.createElement("li");
+//     newList.classList.add("lista");
+//     const spanText = document.createElement("span");
+//     spanText.classList.add("text");
+//     spanText.innerText = textInput.value.trim();
+//     newList.appendChild(spanText);
+//     //creazione icona del check
+//     const selezione = document.createElement("button");
+//     selezione.innerHTML = "<i class='fas fa-check'></i>";
+//     selezione.classList.add("check");
+//     // creazione icona del cestino
+//     const eliminare = document.createElement("button");
+//     eliminare.innerHTML = "<i class='fas fa-trash'></i>";
+//     eliminare.classList.add("trash");
+//     // append.child dei button con icone
+//     const spanButtons = document.createElement("span");
+//     spanButtons.classList.add("span-buttons");
+//     spanButtons.appendChild(selezione);
+//     spanButtons.appendChild(eliminare);
+//     newList.appendChild(spanButtons);
+//     newDiv.appendChild(newList);
+
+//     insertLi.appendChild(newDiv);
+
+//     // pulire l'input
+//     textInput.value = "";
+//   }
+// });
+
 form.addEventListener("submit", function (e) {
-  // PER PRIMISSIMA COSA (SEMPRE) dovremo fermare il comportamento di default del form (il refresh) in questo modo:
   e.preventDefault();
-  // dobbiamo crearci un div che conterrà la lista
-  const newDiv = document.createElement("div");
-  //assegnarci una classe per lo stile
-  newDiv.classList.add("div-lista");
-  // dobbiamo prenderci il dato dell'input
-  if (textInput.value === "") {
-    //messaggio di errore in caso di stringa vuota
+
+  if (textInput.value.trim() === "") {
     alert("ATTENZIONE! Non hai inserito alcun testo!");
   } else {
-    //comandi per la creazione di una nuova lista da inserire in un div
     const newList = document.createElement("li");
-    newList.innerText = textInput.value;
     newList.classList.add("lista");
-    newDiv.appendChild(newList);
-    //creazione icona del check
+
+    const spanText = document.createElement("span");
+    spanText.classList.add("text");
+    spanText.innerText = textInput.value.trim();
+    newList.appendChild(spanText);
+
     const selezione = document.createElement("button");
     selezione.innerHTML = "<i class='fas fa-check'></i>";
     selezione.classList.add("check");
-    newDiv.appendChild(selezione);
-    // creazione icona del cestino
+
     const eliminare = document.createElement("button");
     eliminare.innerHTML = "<i class='fas fa-trash'></i>";
     eliminare.classList.add("trash");
-    newDiv.appendChild(eliminare);
-    //creazioen del ripristino
-    const ripristino = document.createElement("button");
-    ripristino.innerHTML = "<i class='fa fa-undo'></i>";
-    ripristino.classList.add("return");
-    newDiv.appendChild(ripristino);
 
-    insertLi.appendChild(newDiv);
+    const spanButtons = document.createElement("span");
+    spanButtons.classList.add("span-buttons");
+    spanButtons.appendChild(selezione);
+    spanButtons.appendChild(eliminare);
 
-    // pulire l'input
+    newList.appendChild(spanButtons);
+
+    insertLi.appendChild(newList);
+
     textInput.value = "";
   }
-
-  //seconda manipolazione direttamente con i button
-  //   const button = insertLi.querySelector("button");
-  //   button.addEventListener("click", function (e) {
-  //     console.log(e.target.classList);
-  //   });
-  // non puoi manipolare i button ancor prima della creazione
 });
 
 // manipolazione per il check e la rimozione
 insertLi.addEventListener("click", function (e) {
   // risalgo al button
   const button = e.target.closest("button");
+  // esci dalla funzione se non hai cliccato su un bottone
+  if (!button) return;
   // if per capire se il button selezione è check
+  const liCheck = button.closest("li");
   if (button.classList.contains("check")) {
-    const divLi = button.closest(".div-lista");
-    const liCheck = divLi.querySelector("li");
-    liCheck.classList.add("completato");
+    // const divLi = button.closest(".div-lista");
+    liCheck.classList.toggle("completato");
     // else if per capire se è trash
   } else if (button.classList.contains("trash")) {
-    const divLi = button.closest(".div-lista");
-    divLi.remove();
-  } else if (button.classList.contains("return")) {
-    const divLi = button.closest(".div-lista");
-    const liCheck = divLi.querySelector("li");
-    liCheck.classList.toggle("completato");
+    // const divLi = button.closest(".div-lista");
+    liCheck.remove();
   }
 });
 
-// const divReset = document.getElementById("container-list");
-// divReset.addEventListener("click", function (e) {
-//   const button = e.target.closest("button");
-//   if (button.classList.contains("reset")) {
-//     const divLi = divReset.querySelector(".div.lista");
-//     const licheck = divLi.querySelector("li");
-//     licheck.style.textDecoration = "none";
-//   }
-// });
+// reset-bottom
+const containerList = document.getElementById("container-list");
+
+containerList.addEventListener("click", (e) => {
+  const button = e.target.closest("button");
+  if (!button) return;
+  if (button.classList.contains("reset-check")) {
+    const allLi = containerList.querySelectorAll("li");
+    allLi.forEach((element) => element.classList.remove("completato"));
+  } else if (button.classList.contains("remove-all")) {
+    const allLi = containerList.querySelectorAll("li");
+    allLi.forEach((element) => element.remove());
+  }
+});
+
+new Sortable(document.getElementById("todo-list"), {
+  animation: 150,
+});
